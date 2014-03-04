79dad648391b071e0d702f0cfc19861b22fbe4a2
jicui <cui.jiajun@hotmail.com>
2014-03-04 08:28:25 +0000

    add rebase and minor refactor

--- util/version_manager.rb
+++ util/version_manager.rb
@@ -1,50 +1,62 @@
 #!/usr/bin/env ruby
 class ShapeBranch
-  def initialize(url,oldVersion,newVersion)
-    @url=url
-    @oldV=oldVersion
-    @newV=newVersion
+  def initialize(target)
+    @target=target
     @poms=[]
   end
 
   def printVariable
-    p 'url='+@url+' old version='+@oldV+' new version='+@newV
+    p 'target='+@target+' old version='+@oldV+' new version='+@newV
   end
 
-  def _loadPom(url)
-    Dir.foreach(url){|e|
+  def loadPom(target)
+    Dir.foreach(target){|e|
       if e=='.'||e=='..'||e=='target' then next end
-      _f=url+"\\"+e
+      _f=target+"\\"+e
       if e=='pom.xml' then @poms<<_f end
       if File.directory? _f then  _loadPom _f end
     }
   end
 
-  def changeVersion
+  def changeVersion (oldVersion,newVersion)
+    @oldV=oldVersion
+    @newV=newVersion
     printVariable
     a=0
     #load poms
     p 'loading pom.xml'
-    _loadPom @url
+    loadPom @target
     p @poms.length.to_s+' pom.xml loaded'
     #loop pom and replace the old version
     @poms.each{|pom|
       #search and replace
+      p pom
       file=File.read(pom)
       if file.scan(/#{@oldV}/).length<=0 then next end
       buffer=file.gsub(/#{@oldV}/,@newV)
       if not File.writable? pom then
         #check out the file first
-        File.open(pom).chmod(0755)
+        _p4editstr='p4 edit '<<pom
+        exec _p4editstr
       end
-      File.write(pom,buffer)
+        File.write(pom,buffer)
       a+=1
     }
     p a.to_s+' pom.xml converted'
   end
+
+  def rebase (from,to)
+    p4str='p4 integrate -i -Dt '<<from<<'... '<<to<<'...'
+    p p4str
+    exec p4str
+  end
 end
-exec 'ls'
-exec 'pwd'
-shapeBranch=ShapeBranch.new("C:\\dev\\stubhub\\domain\\fulfillment\\pb_fulfillment_revamp", '1.9-SNAPSHOT', '1.pb_fulfillment_revamp.0-SNAPSHOT')
+shapeBranch=ShapeBranch.new("C:\\Users\\jicui\\git\\p4\\sandbox\\jicui\\t2")
 #shapeBranch=ShapeBranch.new("C:\\dev\\stubhub\\domain\\fulfillment\\pb_fulfillment_revamp","1.pb_fulfillment.0-SNAPSHOT","1.9-SNAPSHOT")
-shapeBranch.changeVersion
+shapeBranch.changeVersion '1.pb_fulfillment_revamp.0-SNAPSHOT', '1.9-SNAPSHOT'
+exec 'p4 submit ' #can not submit?
+shapeBranch.rebase '//sandbox/jicui/t1/','//sandbox/jicui/t2/'
+
+#add checkout functions
+#add merge back to main functions
+#expose to gli
