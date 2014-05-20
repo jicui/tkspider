f189d0598c62c36a8e215a4e7b6d52c06c3f1c9e
jicui <cui.jiajun@hotmail.com>
2014-05-20 23:26:33 +0800

    remove uncessary idea local folder and add branch module

--- shape/lib/shape/branch.rb
+++ shape/lib/shape/branch.rb
@@ -1,1 +1,75 @@
-module Branch  def loadPom(target,allPoms)    Dir.foreach(target) { |e|      if e=='.'||e=='..'||e=='target' then next end      _f=target+"\\"+e      if e=='pom.xml' then allPoms<<_f end      if File.directory? _f then  loadPom(_f,allPoms) end    }  end    def prompt(msg)    puts msg    STDIN.gets  end  def updateVersion(path,oldVersion,newVersion)    if(!path) then      path=while line=prompt("Please input Path")             break(line.chomp) if line!="\n"           end    end    if(!oldVersion) then      oldVersion=while line= prompt("Please input version1")                   break(line.chomp) if line!="\n"                 end    end    if(!newVersion) then      newVersion=while line=prompt("Please input verson2")                   break(line.chomp) if line!="\n"                 end    end    puts "Start converting version  from #{oldVersion}  to #{newVersion} on #{path}"    cnt=0    #load poms    puts "loading pom.xml"    allPoms=[]    loadPom(path,allPoms)    puts allPoms.length.to_s+"pom.xml loaded"    #loop pom and replace the old version    allPoms.each do |pom|      #search and replace      file=File.read(pom)      if file.scan(/#{oldVersion}/).length<=0 then next end      buffer=file.gsub(/#{oldVersion}/,newVersion)      if not File.writable? pom then        #check out the file first        puts "p4 edit #{pom}"        `p4 edit #{pom}`      end      File.write(pom,buffer)      cnt+=1    end    p cnt.to_s+' pom.xml converted'  endend
+module Branch
+
+  def Branch.load_pom(target)
+    dir_poms=[]
+    Dir.foreach(target) { |e|
+      if e=='.'||e=='..'||e=='target' then next end
+      file_or_dir=target+"\\"+e
+      if e=='pom.xml' then dir_poms<<file_or_dir end
+      if File.directory? _f then  dir_poms<<loadPom(file_or_dir) end
+    }
+    return dir_poms;
+  end
+  
+
+  def Branch.prompt(msg)
+    puts msg
+    STDIN.gets
+  end
+
+  def Branch.update_version(path,oldVersion,newVersion)
+    if(!path) then
+      path=while line=prompt("Please input Path")
+             break(line.chomp) if line!="\n"
+           end
+    end
+
+    if(!oldVersion) then
+      oldVersion=while line= prompt("Please input version1")
+                   break(line.chomp) if line!="\n"
+                 end
+    end
+
+    if(!newVersion) then
+      newVersion=while line=prompt("Please input verson2")
+                   break(line.chomp) if line!="\n"
+                 end
+    end
+    puts "Start converting version  from #{oldVersion}  to #{newVersion} on #{path}"
+    cnt=0
+    #load poms
+    puts "loading pom.xml"
+    allPoms=load_pom(path)
+    puts allPoms.length.to_s+"pom.xml loaded"
+    #loop pom and replace the old version
+    allPoms.each do |pom|
+      #search and replace
+      file=File.read(pom)
+      if file.scan(/#{oldVersion}/).length<=0 then next end
+      buffer=file.gsub(/#{oldVersion}/,newVersion)
+      if not File.writable? pom then
+        #check out the file first
+        puts "p4 edit #{pom}"
+        `p4 edit #{pom}`
+      end
+      File.write(pom,buffer)
+      cnt+=1
+    end
+    p cnt.to_s+' pom.xml converted'
+  end
+
+  def Branch.integrate(from,to)
+    if(!from) then
+      from=while line=prompt("Input the from branch path")
+             break(line.chomp) if line!="\n"
+           end
+    end
+    if(!to) then
+      to=while line=prompt("Input the to branch path")
+           break(line.chomp) if line!="\n"
+         end
+    end
+    `p4 integrate -i -Dt #{from}... #{to}...`
+  end
+
+end
